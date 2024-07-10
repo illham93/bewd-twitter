d04ca43014fe5b86b9c77d0252e41dac12d2d78f
Devon <devonhamill93@gmail.com>
2024-07-10 13:47:07 -0400

    Migrations done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :username, :presence => true, :length => {minimum: 3, maximum: 64}, :uniqueness => true
+    validates :password, :presence => true, :length => {minimum: 8, maximum: 64}
+    validates :email, :presence => true, :length => {minimum: 5, maximum: 500}
+
+    has_many :sessions
+    has_many :tweets
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2024_07_10_171843) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "token"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "message"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
